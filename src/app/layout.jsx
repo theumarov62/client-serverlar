492397bee868855223b56854c7a79059d1062688
theumarov62 <maxbubaxonxoliqova62@gmail.com>
2026-03-13 15:43:32 +0500

    add b

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import "./globals.css";
 
-function layout() {
-  return <></>;
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
-
-export default layout;
